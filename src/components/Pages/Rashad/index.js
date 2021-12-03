8a2bfbb481958d14721b8699613a0b6da6255558
Brandon Chun <zeonbravo@gmail.com>
2021-12-03 00:57:38 -0800

    starter

--- src/components/Pages/Rashad/index.js
+++ src/components/Pages/Rashad/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { GlobalStyle } from './globalStyles';
 import Hero from './Hero';
 import Products from './Products';
-import Feature from './Feature';
+import Feature from './Feature/Rashad';
 import Footer from './Footer';
 import { albumData, recommendedArtists } from './Products/data';
 
